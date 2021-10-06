21a917544b35e65eba82492dee967c0a818f120a
luisluft <luberluft@gmail.com>
2021-10-06 11:04:19 -0400

    add first two apps

--- src/App.js
+++ src/App.js
@@ -5,11 +5,22 @@ import { Cards } from "react-responsive-cards";
 const App = () => {
   const details = [
     {
-      title: "The Card Title",
-      description: "This is a short description",
-      image: "https://<image_here>.jpg",
-      renderFooter: <div>{"Custom JSX"}</div>,
-      handleOnClick: () => alert("Custom Event"),
+      title: "react-cocktails",
+      description:
+        "Simple react app used to learn how to create a search UI for a cocktails API",
+      image: "https://i.imgur.com/ofZfXM3.png",
+      handleOnClick: () =>
+        window
+          .open("https://github.com/luisluft/react-cocktails", "_blank")
+          .focus(),
+    },
+    {
+      title: "react-cart",
+      description:
+        "Simple react app used to learn how to create a shopping cart.",
+      image: "https://i.imgur.com/n1eah0J.png",
+      handleOnClick: () =>
+        window.open("https://github.com/luisluft/react-cart", "_blank").focus(),
     },
   ];
 
